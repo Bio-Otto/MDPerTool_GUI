654e5e41abc0bf2d530578bb3479e9baf3dd5fd2
HIbrahim <halil.ibrahim.oozdemir@gmail.com>
2021-12-24 16:22:06 +0300

    :+1:

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         ],
     },
     install_requires=install_requires,
-    python_requires='>=3.7, <=3.9',
+    python_requires='>=3.5, <=3.9',
     tests_require=tests_require,
     setup_requires=setup_requires,
     # dependency_links=[
