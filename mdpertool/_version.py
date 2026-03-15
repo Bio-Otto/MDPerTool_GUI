edcfef697de6993b0240f6eea13882385a743244
HIbrahim <57147948+Bio-Otto@users.noreply.github.com>
2026-03-16 01:26:36 +0300

    Bump version to 0.0.2.dev1 and tighten conda recipe tests

--- mdpertool/_version.py
+++ mdpertool/_version.py
@@ -3,7 +3,7 @@ import os.path as op
 global readme, changelog, __citation__, __long_description__, __url__, __author_email__
 
 
-__version_info__ = ('0', '0', '1', 'dev4')
+__version_info__ = ('0', '0', '2', 'dev1')
 
 __author__ = 'Halil I. Ozdemir'
 
